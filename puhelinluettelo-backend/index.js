bc839bb12029393a4b9d661d0fef3494cef68676
Misukisti <misukisti@gmail.com>
2025-07-10 16:39:56 +0300

    Lisätty frontendin tuotantoversio

--- puhelinluettelo-backend/index.js
+++ puhelinluettelo-backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
+const path = require('path')
 
 app.use(cors())
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, '../puhelinluettelo-frontedn/dist')))
 
 let persons = [
   { id: 1, name: 'Arto Hellas', number: '040-123456' },
