dbdbf236d7bdaab99c12bff3cf48b0395d98265a
Misukisti <misukisti@gmail.com>
2025-07-10 18:36:10 +0300

    pliiis

--- puhelinluettelo-frontedn/src/App.jsx
+++ puhelinluettelo-frontedn/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const baseUrl = import.meta.env.VITE_BACKEND_URL
+const baseUrl = '/api/persons'
 
 function App() {
   const [persons, setPersons] = useState([])
--- puhelinluettelo-frontedn/vite.config.js
+++ puhelinluettelo-frontedn/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://puhelinluettelo-backend-ivdg.onrender.com',
+        target: 'https://puhelinluettelo-backend-ivdg.onrender.com/',
         changeOrigin: true,
       },
     },
